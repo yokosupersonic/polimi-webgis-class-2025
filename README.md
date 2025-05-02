f197ae4e46ed7ff07d532386b648398330116b28
Juan Pablo Duque <juan-pablo-duque@hotmail.com>
2025-05-02 20:11:06 +0200

    Adding project structure

---
+++ README.md
@@ -0,0 +1,55 @@
+# GIS 2024 - Politecnico di Milano
+Repository for the Web Development part of the GIS lessons 2023 - 2024.
+
+The contents of this repository are entirely for learning purposes. All contents and code is completely open and available and based on open-source software.
+
+Students are encouraged to fork and and modify this template as much as they want.
+
+## Installing
+Clone the repository using the command 
+```sh
+git clone https://github.com/Diuke/polimi-webgis-class-2025 
+```
+
+Or fork the repository for having your own copy.
+
+This application uses npm as the package manager and Vite as the web server.
+It uses Node version 16 or higher.
+
+First, install the dependencies using the command:
+```sh
+npm install
+```
+
+Run the project using the command.
+```sh
+npm run start
+```
+
+This will start a Vite server. Open the application in http://localhost:5173/
+
+Using Vite it is possible to follow the examples of OpenLayers, as well as use more advanced TypeScript and Visual Studio Code capabilities, such 
+as intellisense, fast reload, documentation, and code completion.
+
+## Building
+Run the command 
+```sh
+npm run build
+```
+
+This will re-create the folder /docs with all the necessary files for hosting the website in GitHub Pages.
+
+Modify the configuration file (vite.config.js) for different build configurations and folder structures.
+This is a dummy project and is not intended to be a one-fits-all configuration.
+
+## Credits
+The website is based on the template:
+
+Twenty by HTML5 UP
+html5up.net | @ajlkn
+Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+Adaptation to Vite and OpenLayers by Diuke
+https://github.com/Diuke
+
+
---
+++ assets/js/map.js
@@ -0,0 +1,277 @@
+import 'ol/ol.css';
+import 'ol-layerswitcher/dist/ol-layerswitcher.css';
+import { Map, View, Overlay } from 'ol';
+import { Tile, Image, Group, Vector } from 'ol/layer';
+import { OSM, ImageWMS, BingMaps, StadiaMaps } from 'ol/source';
+import VectorSource from 'ol/source/Vector';
+import { GeoJSON } from 'ol/format';
+import { fromLonLat } from 'ol/proj';
+import { ScaleLine, FullScreen, MousePosition, } from 'ol/control';
+import LayerSwitcher from 'ol-layerswitcher';
+import { createStringXY } from 'ol/coordinate';
+import { Style, Stroke, Fill } from 'ol/style';
+
+let osm = new Tile({
+    title: "Open Street Map",
+    type: "base",
+    visible: true,
+    source: new OSM()
+});
+let colombiaBoundary = new Image({
+    title: "Colombia Administrative level 0",
+    source: new ImageWMS({
+        url: 'https://www.gis-geoserver.polimi.it/geoserver/wms',
+        params: { 'LAYERS': 'gis:COL_adm0', 'STYLES': 'restricted' }
+    })
+});
+var colombiaDepartments = new Image({
+    title: "Colombia Administrative level 1",
+    source: new ImageWMS({
+        url: 'https://www.gis-geoserver.polimi.it/geoserver/wms',
+        params: { 'LAYERS': 'gis:COL_adm1' }
+    }),
+    opacity: 0.5
+});
+
+var colombiaRoads = new Image({
+    title: "Colombia Roads",
+    source: new ImageWMS({
+        url: 'https://www.gis-geoserver.polimi.it/geoserver/wms',
+        params: { 'LAYERS': 'gis:COL_roads' }
+    }),
+    visible: false
+});
+
+var colombiaRivers = new Image({
+    title: "Colombia Rivers",
+    source: new ImageWMS({
+        url: 'https://www.gis-geoserver.polimi.it/geoserver/wms',
+        params: { 'LAYERS': 'gis:COL_rivers' }
+    }),
+    minResolution: 1000,
+    maxResolution: 5000
+});
+
+//Create the layer groups and add the layers to them
+let basemapLayers = new Group({
+    title: "Base Maps",
+    layers: [osm]
+})
+
+let overlayLayers = new Group({
+    title: "Overlay Layers",
+    layers: [
+        colombiaBoundary, 
+        colombiaDepartments, 
+        colombiaRivers, 
+        colombiaRoads
+    ]
+})
+
+// Map Initialization
+let mapOrigin = fromLonLat([-74, 4.6]);
+let zoomLevel = 5;
+let map = new Map({
+    target: document.getElementById('map'),
+    layers: [basemapLayers, overlayLayers],
+    view: new View({
+        center: mapOrigin,
+        zoom: zoomLevel
+    }),
+    projection: 'EPSG:3857'
+});
+
+// Add the map controls:
+// map.addControl(new ScaleLine());
+// map.addControl(new FullScreen());
+// map.addControl(new MousePosition({
+//     coordinateFormat: createStringXY(4),
+//     projection: 'EPSG:4326',
+//     className: 'custom-control',
+//     placeholder: '0.0000, 0.0000'
+// }));
+
+// let layerSwitcher = new LayerSwitcher({});
+// map.addControl(layerSwitcher);
+
+//Add the Stadia Maps layers
+// var stadiaWatercolor = new Tile({
+//     title: "Stadia Watercolor",
+//     type: "base",
+//     visible: false,
+//     source: new StadiaMaps({
+//         layer: 'stamen_watercolor'
+//     })
+// })
+// var stadiaToner = new Tile({
+//     title: "Stadia Toner",
+//     type: "base",
+//     visible: false,
+//     source: new StadiaMaps({
+//         layer: 'stamen_toner'
+//     })
+// })
+// basemapLayers.getLayers().extend([stadiaWatercolor, stadiaToner]);
+
+//OPTIONAL
+//Add the Bing Maps layers
+// var BING_MAPS_KEY = "PUT_YOUR_BING_MAPS_KEY_HERE"; //Replace with your Bing Maps key
+// var bingRoads = new Tile({
+//     title: 'Bing Maps—Roads',
+//     type: 'base',
+//     visible: false,
+//     source: new BingMaps({
+//         key: BING_MAPS_KEY,
+//         imagerySet: 'Road'
+//     })
+// });
+
+// var bingAerial = new Tile({
+//     title: 'Bing Maps—Aerial',
+//     type: 'base',
+//     visible: false,
+//     source: new BingMaps({
+//         key: BING_MAPS_KEY,
+//         imagerySet: 'Aerial'
+//     })
+// });
+// basemapLayers.getLayers().extend([bingRoads, bingAerial]);
+
+// Add a static GeoJSON file to the map
+// let staticGeoJSONSource = new VectorSource({
+//     url: '../geojson/COL_adm2.geojson', // Replace with the actual path to your GeoJSON file
+//     format: new GeoJSON()
+// });
+
+// let staticGeoJSONLayer = new Vector({
+//     title: "Static GeoJSON Layer",
+//     source: staticGeoJSONSource,
+//     style: new Style({
+//         stroke: new Stroke({
+//             color: "#ff7f50",
+//             width: 2
+//         }),
+//         fill: new Fill({
+//             color: "rgba(255, 127, 80, 0.5)"
+//         })
+//     })
+// });
+// overlayLayers.getLayers().extend([staticGeoJSONLayer]);
+
+//Add the WFS layer
+// let wfsSource = new VectorSource()
+// let wfsLayer = new Vector({
+//     title: "Colombia water areas",
+//     source: wfsSource,
+
+//     style: new Style({
+//         stroke: new Stroke({
+//             color: "#a2d2ff",
+//             width: 1
+//         }),
+//         fill: new Fill({
+//             color: "#bde0fe"
+//         })
+//     }),
+//     zIndex: 9999
+// });
+// overlayLayers.getLayers().extend([wfsLayer]);
+
+// This allows to use the function in a callback!
+// function loadFeatures(response) {
+//     wfsSource.addFeatures(new GeoJSON().readFeatures(response))
+// }
+// // This is not a good practice, but works for the jsonp.
+// window.loadFeatures = loadFeatures;
+
+
+// var base_url = "https://www.gis-geoserver.polimi.it/geoserver/gis/ows?";
+// var wfs_url = base_url;
+// wfs_url += "service=WFS&"
+// wfs_url += "version=2.0.0&"
+// wfs_url += "request=GetFeature&"
+// wfs_url += "typeName=gis%3ACOL_water_areas&"
+// wfs_url += "outputFormat=text%2Fjavascript&"
+// wfs_url += "srsname=EPSG:3857&"
+// wfs_url += "format_options=callback:loadFeatures"
+
+// console.log(wfs_url);
+
+// map.once('postrender', (event) => {
+//     // Load the WFS layer
+//     $.ajax({ url: wfs_url, dataType: 'jsonp' });
+    
+// })
+
+//Add the code for the Pop-up
+// var container = document.getElementById('popup');
+// var content = document.getElementById('popup-content');
+// var closer = document.getElementById('popup-closer');
+
+// var popup = new Overlay({
+//     element: container
+// });
+// map.addOverlay(popup);
+
+// // The click event handler for closing the popup.
+// // This ensures that JQuery ($) is already available in the page.
+// $(document).ready(function () {
+//     map.on('singleclick', function (event) {
+//         //This iterates over all the features that are located on the pixel of the click (can be many)
+//         var feature = map.forEachFeatureAtPixel(event.pixel, function (feature, layer) {
+//             return feature;
+//         });
+
+//         //If there is a feature, open the popup by setting a position to it and put the data from the feature
+//         if (feature != null) {
+//             var pixel = event.pixel;
+//             var coord = map.getCoordinateFromPixel(pixel);
+//             popup.setPosition(coord);
+//             content.innerHTML =
+//                 '<h5>Colombia Water Areas</h5><br><b>Name: </b>' +
+//                 feature.get('NAME') +
+//                 '</br><b>Description: </b>' +
+//                 feature.get('HYC_DESCRI');
+//         } else {
+//             //Only if the colombiaRoads layer is visible, do the GetFeatureInfo request
+//             if (colombiaRoads.getVisible()) {
+//                 var viewResolution = (map.getView().getResolution());
+//                 var url = colombiaRoads.getSource().getFeatureInfoUrl(event.coordinate, viewResolution, 'EPSG:3857', { 'INFO_FORMAT': 'text/html' });
+//                 console.log(url);
+
+//                 if (url) {
+//                     var pixel = event.pixel;
+//                     var coord = map.getCoordinateFromPixel(pixel);
+//                     popup.setPosition(coord);
+//                     //We do again the AJAX request to get the data from the GetFeatureInfo request
+//                     $.ajax({ url: url })
+//                         .done((data) => {
+//                             //Put the data of the GetFeatureInfo response inside the pop-up
+//                             //The data that arrives is in HTML
+//                             content.innerHTML = data;
+//                         });
+//                 }
+//             }
+//         }
+//     });
+// });
+
+
+
+// // Adding map event for pointermove
+// // The click event handler for closing the popup.
+// closer.onclick = function () {
+//     popup.setPosition(undefined);
+//     closer.blur(); 
+//     return false;
+// };
+
+// map.on('pointermove', function(event){
+//     var pixel = map.getEventPixel(event.originalEvent);
+//     var hit = map.hasFeatureAtPixel(pixel);
+//     map.getTarget().style.cursor = hit ? 'pointer' : '';
+// });
+
+// map.on('moveend', function(event){
+//     console.log("moved map");
+// });
